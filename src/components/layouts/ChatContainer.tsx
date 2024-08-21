485be5c3f57c9b0263d5b973b6672b8f6330a210
Daniel Stals <danielstals@gmail.com>
2024-08-21 15:04:07 +0200

    feat: setup functional basic chatbot

--- src/components/layouts/ChatContainer.tsx
+++ src/components/layouts/ChatContainer.tsx
@@ -1,74 +1,81 @@
 'use client';
 
-import { sendPrompt } from '@/lib/ai';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 import { Button } from '../ui/Button';
 import { useToast } from '../ui/use-toast';
 import { PromptSuggestions } from './PromptSuggestions';
 
-import { continueConversation } from '@/app/actions';
-import { CoreMessage } from 'ai';
-import { readStreamableValue } from 'ai/rsc';
+import { Message, useChat } from 'ai/react';
 
 export function ChatContainer(): JSX.Element {
+	const { messages, input, handleInputChange, handleSubmit } = useChat({
+		maxToolRoundtrips: 2,
+	});
+
 	const { toast } = useToast();
 
-	const [messages, setMessages] = useState<CoreMessage[]>([]);
-	const [input, setInput] = useState('');
+	// const [messages, setMessages] = useState<CoreMessage[]>([]);
+	// const [input, setInput] = useState('');
 
 	useEffect(() => {
 		console.log('messages', messages);
 	}, [messages]);
 
 	function setSuggestedPromptHandler(suggestedPrompt: string): void {
-		setInput(suggestedPrompt);
+		// setInput(suggestedPrompt);
 	}
 
 	async function sendPromptHandler(): Promise<void> {
-		const result = await sendPrompt(input);
-		console.log('result', result);
-
-		// toast({ title: 'New toast message', description: 'You have a new message.' });
+		// const result = await sendPrompt(input);
+		// console.log('result', result);
 	}
 
-	async function onSubmitHandler(e: React.FormEvent<HTMLFormElement>): Promise<void> {
-		e.preventDefault();
-		const newMessages: CoreMessage[] = [...messages, { content: input, role: 'user' }];
+	// async function onSubmitHandler(e: React.FormEvent<HTMLFormElement>): Promise<void> {
+	// 	e.preventDefault();
+	// 	const newMessages: CoreMessage[] = [...messages, { content: input, role: 'user' }];
 
-		setMessages(newMessages);
-		setInput('');
+	// 	setMessages(newMessages);
+	// 	setInput('');
 
-		const result = await continueConversation(newMessages);
+	// 	const result = await continueConversation(newMessages);
 
-		for await (const content of readStreamableValue(result)) {
-			setMessages([
-				...newMessages,
-				{
-					role: 'assistant',
-					content: content as string,
-				},
-			]);
-		}
-	}
+	// 	for await (const content of readStreamableValue(result)) {
+	// 		setMessages([
+	// 			...newMessages,
+	// 			{
+	// 				role: 'assistant',
+	// 				content: content as string,
+	// 			},
+	// 		]);
+	// 	}
+	// }
 
 	return (
 		<>
 			<PromptSuggestions className='mb-5' onClick={setSuggestedPromptHandler} />
 
-			<div className='flex flex-col w-full max-w-md py-24 mx-auto stretch'>
-				{messages.map((m, i) => (
-					<div key={i} className='whitespace-pre-wrap'>
-						{m.role === 'user' ? 'User: ' : 'AI: '}
-						{m.content as string}
+			<div className='space-y-4'>
+				{messages.map((m: Message) => (
+					<div key={m.id} className='whitespace-pre-wrap'>
+						<div>
+							<div className='font-bold'>{m.role}</div>
+							<p>
+								{m.content.length > 0 ? (
+									m.content
+								) : (
+									<span className='italic font-light'>{'calling tool: ' + m?.toolInvocations?.[0].toolName}</span>
+								)}
+							</p>
+						</div>
 					</div>
 				))}
 			</div>
 
-			<form className='relative w-full rounded-md shadow' onSubmit={onSubmitHandler}>
+			<form className='relative w-full rounded-md shadow' onSubmit={handleSubmit}>
 				<input
 					value={input}
-					onChange={(e) => setInput(e.target.value)}
+					onChange={handleInputChange}
 					className='w-full p-4 rounded-md outline-none focus:border-primary'
 					type='text'
 					placeholder='Stel me een vraag...'
