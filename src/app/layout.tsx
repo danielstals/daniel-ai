27b69347dea46495d776beebf6cdc4c010f209ef
Daniel Stals <danielstals@gmail.com>
2024-09-04 16:26:28 +0200

    feat: implement vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
@@ -34,6 +35,7 @@ export default function RootLayout({
 					</main>
 				</Providers>
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
