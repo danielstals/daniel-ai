7e3544bc970b1961897ddf7d8750c0e821e56d02
Daniel Stals <danielstals@gmail.com>
2024-09-12 16:57:34 +0200

    feat: add loaders

--- src/components/layouts/chat-layout/chat-layout.tsx
+++ src/components/layouts/chat-layout/chat-layout.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
 import { ReactNode, Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
@@ -9,17 +6,15 @@ type LayoutProps = {
 };
 
 export function ChatLayout({ children }: LayoutProps): JSX.Element {
-	const pathname = usePathname();
-
 	return (
 		<Suspense
 			fallback={
 				<div className='flex size-full items-center justify-center'>
-					<h3>Loading..</h3>
+					<h3>Loading chat..</h3>
 				</div>
 			}
 		>
-			<ErrorBoundary key={pathname} fallback={<div>Something went wrong!</div>}>
+			<ErrorBoundary key='chat' fallback={<div>Something went wrong!</div>}>
 				<div className='flex flex-col overflow-y-hidden max-sm:grow'>{children}</div>
 			</ErrorBoundary>
 		</Suspense>
